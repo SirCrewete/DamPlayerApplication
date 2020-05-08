cdd73d0d69eb96e60996953698b534fe26b36248
sircrewete <gimenocrew1996@gmail.com>
2020-05-08 18:01:41 +0200

    solucion errores 2

--- app/src/main/java/com/example/reproductormusicaapp/insideApp.java
+++ app/src/main/java/com/example/reproductormusicaapp/insideApp.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class insideApp extends AppCompatActivity {
 
